72dfcfce647e69fcee0036723a82f221915f51ba
Alli-anne <145496176+Alli-anne@users.noreply.github.com>
2025-09-24 21:45:10 -0600

    cart fix

--- src/js/product.js
+++ src/js/product.js
@@ -1,8 +1,11 @@
-import { setLocalStorage } from "./utils.mjs";
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
-
 function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
+  // get existing cart or start with empty array
+  const cart = getLocalStorage("so-cart") || [];
+  cart.push(product);
+
+  setLocalStorage("so-cart", cart);
 }
 // add to cart button event handler
 async function addToCartHandler(e) {
