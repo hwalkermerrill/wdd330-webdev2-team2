313475089ae00bb6291a692e17ac33506ac1e1f5
Alli-anne <145496176+Alli-anne@users.noreply.github.com>
2025-10-03 19:10:25 -0600

    cart animation added

--- src/js/product.js
+++ src/js/product.js
@@ -1,5 +1,6 @@
 import { setLocalStorage, getLocalStorage } from "./utils.mjs";
 import { findProductById } from "./productData.mjs";
+
 function addProductToCart(product) {
   // get existing cart or start with empty array
   const cart = getLocalStorage("so-cart") || [];
@@ -11,9 +12,20 @@ function addProductToCart(product) {
 async function addToCartHandler(e) {
   const product = await findProductById(e.target.dataset.id);
   addProductToCart(product);
+  cartAnimation();
 }
+
+function cartAnimation() {
+  document.querySelector(".cart").classList.add("animate");
+  setTimeout(() => {
+    document.querySelector(".cart").classList.remove("animate");
+  }, 1000);
+}
+
+
 
 // add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+  
