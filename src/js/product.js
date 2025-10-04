ba564c6a51eb58174303aa0936b318c5e2e199b7
Alli-anne <145496176+Alli-anne@users.noreply.github.com>
2025-10-04 12:00:10 -0600

    Don't merge not working. Working on removing things from cart. I did it under the incorrect branch.

--- src/js/product.js
+++ src/js/product.js
@@ -22,10 +22,54 @@ function cartAnimation() {
   }, 1000);
 }
 
+function renderCart(){
+  const cartContainer = document.getElementById("cart");
+  cartContainer.innerHTML = ""; // clear old list
 
+  const cart = getLocalStorage("so-cart") || [];
+  
+  cart.forEach(item => {
+    const cartItem = document.createElement("li");
+    cartItem.classList.add("cart-card");
+    cartItem.innerHTML = cartItemTemplate(item);
+    cartContainer.appendChild(cartItem);
+  });
 
+}
+
+function removeFromCartButton(){
+  document.querySelectorAll(".removeFromCart").forEach(btn => console.log(btn.dataset.id));
+  document.querySelectorAll(".removeFromCart").forEach(button => {
+    button.addEventListener("click", (e) => {
+      console.log(e.target);
+      const id = e.target.getAttribute('data-id');
+      removeFromCart(id);
+      renderCart();
+    });
+  })
+}
+
+function removeFromCart(id) {
+  const cart = getLocalStorage("so-cart") || [];
+  const newCart = cart.filter((item) => String(item.Id) !== String(id));
+  setLocalStorage("so-cart", newCart);
+  
+}
+
+const cartContainer = document.querySelector(".products");
+console.log(cartContainer);
+
+cartContainer.addEventListener("click", (e) => {
+  if (e.target.classList.contains("removeFromCart")) {
+    const id = e.target.getAttribute('data-id');
+    removeFromCart(id);
+    renderCart();
+  }
+})
 // add listener to Add to Cart button
 document
   .getElementById("addToCart")
   .addEventListener("click", addToCartHandler);
+  
+  
   
