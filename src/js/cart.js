f26a360c20af06a1335c0454709d8d85b9058d56
gagedorl <gagedorlando@gmail.com>
2025-10-04 10:46:00 -0600

    did total in cart task

--- src/js/cart.js
+++ src/js/cart.js
@@ -1,9 +1,22 @@
 import { getLocalStorage } from "./utils.mjs";
 
+let totalCost = 0;
+
 function renderCartContents() {
   const cartItems = getLocalStorage("so-cart");
+  if (cartItems && cartItems.length > 0) {
+    cartItems.forEach(item =>{
+      totalCost += item.FinalPrice;
+    })
+  }
   const htmlItems = cartItems.map((item) => cartItemTemplate(item));
   document.querySelector(".product-list").innerHTML = htmlItems.join("");
+  document.getElementById("total-price").textContent = `$${totalCost.toFixed(2)}`;
+  if(totalCost > 0){
+    document.querySelector(".cart-total").classList.remove("hide");
+  } else{
+    document.querySelector(".cart-total").classList.add("hide");
+  }
 }
 
 function cartItemTemplate(item) {
