8e46fe758515802738d67c74b5c9225c12b36a98
autumnsroundy <rou24001@byui.edu>
2025-10-07 19:25:41 -0800

    Final updates for merge to main

---
+++ src/js/productList.mjs
@@ -0,0 +1,23 @@
+import { getData } from "./productData.mjs";
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+    return `<li class="product-card">
+    <a href="product_pages/index.html?product=">
+    <img
+      src="${product.Image}"
+      alt="Image of ${product.Name}"
+    />
+    <h3 class="card__brand">${product.Brand.Name}</h3>
+    <h2 class="card__name">${product.NameWithoutBrand}</h2>
+    <p class="product-card__price">$${product.FinalPrice}</p></a>
+  </li>`
+}
+
+export default function productList(selector, category) {
+   const el = document.querySelector(selector);
+   const products = getData(category);
+   console.log(products);
+    renderListWithTemplate(productCardTemplate, el, products);
+}
+
