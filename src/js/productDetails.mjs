fc7bcf401458df8a7e8b61a2a162c9b8a522d408
gagedorl <gagedorlando@gmail.com>
2025-10-11 11:20:15 -0600

    added discount indicators to product pages and product list

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -24,6 +24,11 @@ export function renderProductDetails(){
     document.getElementById("productName").textContent = product.Name;
     document.getElementById("productNameWithoutBrand").textContent = product.NameWithoutBrand;
     document.getElementById("productImage").src = product.Image;
+    document.getElementById("productImage").alt = product.Name;
+    let discount = product.ListPrice - product.FinalPrice;
+    if (discount > 0) {
+        document.getElementById("productDiscount").textContent = `You save $${discount.toFixed(2)}!`;
+    }
     document.getElementById("productFinalPrice").textContent = `$${product.FinalPrice}`;
     document.getElementById("productColorName").textContent = product.Colors[0].ColorName;
     document.getElementById("productDescriptionHtmlSimple").innerHTML = product.DescriptionHtmlSimple;
